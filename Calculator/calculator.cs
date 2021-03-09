a35861005d5cebdb2849d93beaf43a8f6d35e2a7
Gitou <sigitcold007@gmail.com>
2021-03-09 14:52:37 +0700

    Menambahkan File Calculator

--- Calculator/calculator.cs
+++ Calculator/calculator.cs
@@ -3,4 +3,14 @@ class Calculator
 	static void Main(string[] argas_
 	{
 	}
+	
+	public int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan)int a, int b)
+	{
+		return a- b;
+	}
 }
